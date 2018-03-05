3752eed557400a6f13645e828c67134bc30a13fa
webeginer <webeginer@yandex.ru>
2018-03-05 12:07:03 +0500

    Удаление default в sites-available

--- etc/init.sh
+++ etc/init.sh
@@ -2,7 +2,7 @@
 
 sudo mv /home/box/web/etc/nginx.conf  /etc/nginx/sites-available/frontend
 sudo ln -s /etc/nginx/sites-available/frontend /etc/nginx/sites-enabled/
-sudo rm -rf /etc/nginx/sites-enabled/default
+sudo rm -rf /etc/nginx/sites-available/default
 sudo /etc/init.d/nginx restart
 # sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
 # sudo /etc/init.d/gunicorn restart
