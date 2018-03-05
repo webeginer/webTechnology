681bfb64ecd5415d329e4009cd5cebda0e64c29a
webeginer <webeginer@yandex.ru>
2018-03-05 23:20:51 +0500

    Добавление, переименование, исправление

--- hello.py
+++ hello.py
@@ -4,3 +4,12 @@ origin = QUERY_STRING
 param_list = origin.split('&')
 for x in param_list:
 	print(x)
+
+def hello(environ, start_response):
+	status = '200 OK'
+	headers = [
+		('Content-Type', 'text/plain')
+	]
+	body = 'Hello, world!'
+	start_response(status, headers)
+	return [ body ]
