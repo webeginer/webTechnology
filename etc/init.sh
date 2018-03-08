aa979789ab2df826a0105f25295b85cb7787cc9d
webeginer <webeginer@yandex.ru>
2018-03-08 18:22:24 +0500

    Добавление Джанго, создание проекта ask, приложения qa, изменение конфига ngix

--- etc/init.sh
+++ etc/init.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-sudo mv /home/box/web/etc/reverseProxy  /etc/nginx/sites-available/default
-sudo ln -s /etc/nginx/sites-available/reverseProxy /etc/nginx/sites-enabled/
+sudo mv /home/box/web/etc/django  /etc/nginx/sites-available/default
+sudo ln -s /etc/nginx/sites-available/django /etc/nginx/sites-enabled/
 sudo /etc/init.d/nginx restart
-sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
-sudo /etc/init.d/gunicorn restart
+# sudo /etc/init.d/gunicorn restart
 # sudo /etc/init.d/mysql start
+
+# вызвать команду в корне проекта, где находиться файл manage.py
+# gunicorn ask.wsgi:application --bind 127.0.0.1:8000
