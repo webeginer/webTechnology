52ed1cf0ed870e1f06e5e1c7449fe732c2f4f420
webeginer <webeginer@yandex.ru>
2018-03-22 12:08:00 +0500

    Ревёрсинг в модели Вопрос

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -3,7 +3,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
-
+# from django.core.urlresolvers import reverse
 
 
 # Question - вопрос
@@ -27,6 +27,32 @@ class QuestionManager(models.Manager):
 			return self.order_by('rating')		
 
 
+# class Question(models.Model):
+# 	title = models.CharField(max_length=100)
+# 	text = models.TextField()
+# 	added_at = models.DateTimeField(
+# 		'date published',
+# 		auto_now_add=True,
+# 		)
+# 	rating = models.IntegerField(default=0)
+# 	author = models.ForeignKey(
+# 		User,
+# 		related_name='question_author',
+# 		null=True,
+# 		)
+# 	likes = models.ManyToManyField(
+# 		User,
+# 		related_name='question_likes',
+# 		blank=True,
+# 	)
+# 	def get_url(self):
+# 		return reverse('qa:question', kwargs={'id': self.id})
+# 	def __str__(self):
+# 		return self.title
+# 	def __str__(self):
+# 		return self.text
+# 	objects = QuestionManager()
+
 class Question(models.Model):
 	title = models.CharField(max_length=100)
 	text = models.TextField()
@@ -45,20 +71,12 @@ class Question(models.Model):
 		related_name='question_likes',
 		blank=True,
 	)
-	def get_url(self):
-		return reverse('qa:question', kwargs={'id': self.id})
-	def __str__(self):
-		return self.title
-	def __str__(self):
-		return self.text
 	objects = QuestionManager()
-
-
-# Answer - ответ
-# text - текст ответа
-# added_at - дата добавления ответа
-# question - вопрос, к которому относится ответ
-# author - автор ответа
+Answer - ответ
+text - текст ответа
+added_at - дата добавления ответа
+question - вопрос, к которому относится ответ
+author - автор ответа
 
 
 class Answer(models.Model): 
