998ea8cbf6141e14de8360b8c49e16e428c73755
webeginer <webeginer@yandex.ru>
2018-03-24 12:25:09 +0500

    Удаление очепятки

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -13,7 +13,5 @@ urlpatterns = [
 	url(r'popular/', views.popular, name='popular'),
 	# ex: /question/5/
 	url(r'^question/(?P<question_id>[0-9]+)/$', views.question, name='question'),
-	# ex: /qusetion/5/
-	url(r'^qusetion/(?P<question_id>[0-9]+)/$', views.question, name='question'),
 ]
 
