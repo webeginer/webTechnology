85aeb2cf53f3d7ca1560e7a2538a5d9d78b68671
webeginer <webeginer@yandex.ru>
2018-03-24 10:58:47 +0500

    Qusetion

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -13,4 +13,7 @@ urlpatterns = [
 	url(r'popular/', views.popular, name='popular'),
 	# ex: /question/5/
 	url(r'^question/(?P<question_id>[0-9]+)/$', views.question, name='question'),
+	# ex: /qusetion/5/
+	url(r'^qusetion/(?P<question_id>[0-9]+)/$', views.question, name='question'),
 ]
+
